8849f228653b42b7a92a82201300e4ee02edecc2
Harmony <harmonymukolwe@gmail.com>
2024-07-07 22:58:06 +0300

    Adds DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+  const paragraph = document.querySelector("#text");
+  paragraph.textContent = "This is really cool!";
+});
